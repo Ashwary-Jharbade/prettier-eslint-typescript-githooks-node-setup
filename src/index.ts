7d4294ca4d14eb7ba76487e8501adae62b52c219
Ashwary <ashwary.jharbade999@gmail.com>
2021-10-28 16:34:12 +0530

    Added log

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const main = (): void => {
   var b = Object.assign({}, a, { k3: 'v3' });
   console.log(b);
   const app = express();
+  console.log();
 
   for (const i of a) {
     console.log(i);
